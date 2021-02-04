dad386c0853afa9317a2ea912676da61dfc93f4a
Eloy Torres Sánchez <eloytorres39@gmail.com>
2021-02-04 16:00:12 +0100

    Añadido componente Numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
 import React from 'react'
-import Button from './components/Button'
 import Functions from './components/Functions'
+import Numbers from './components/Numbers'
 import MathOperations from './components/MathOperations'
 import Result from './components/Result'
 import './App.css'
 
 const App = () => {
     // las arrow function si solo tienen un parametro se puede quitar los paréntesis
-    const clickHandlerFunction = text => {
-        console.log("Button.clickHandler", text)
-    }
 
     return (<main className="react-calculator">
         <h2>Calc App</h2>
         <div className="d-flex">
             <div className="column-left">
                 <Result value={undefined}/>
-                <div className="numbers">
-                    <Button text="1" clickHandler={clickHandlerFunction}/>
-                    <Button text="2" clickHandler={clickHandlerFunction}/>
-                    <Button text="3" clickHandler={clickHandlerFunction}/>
-                    <Button text="4" clickHandler={clickHandlerFunction}/>
-                    <Button text="5" clickHandler={clickHandlerFunction}/>
-                    <Button text="6" clickHandler={clickHandlerFunction}/>
-                    <Button text="7" clickHandler={clickHandlerFunction}/>
-                    <Button text="8" clickHandler={clickHandlerFunction}/>
-                    <Button text="9" clickHandler={clickHandlerFunction}/>
-                    <Button text="0" clickHandler={clickHandlerFunction}/>
-                </div>
+                <Numbers onClickNumber={number => console.log("Click en number: ", number)} />
                 <Functions
                     onContentClear={clear => console.log("Content Clear", clear)}
                     onDelete={() => console.log("onDelete")}
